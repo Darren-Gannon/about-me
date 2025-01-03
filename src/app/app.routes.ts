9b0f9f12d87df5dded57827f24ec90d6b406f559
darren <darrengannon93@gmail.com>
2025-01-03 12:53:34 +0200

    fix: tried moving background image

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,5 @@ import { Routes } from '@angular/router';
 import { CvPageComponent } from './pages/cv-page/cv-page.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'cv', pathMatch: 'full' },
-    { path: 'cv', component: CvPageComponent },
+    { path: '', pathMatch: 'full', component: CvPageComponent },
 ];
